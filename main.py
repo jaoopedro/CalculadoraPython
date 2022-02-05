914084f77b464c42c4b21408affc5e2ebe510d2d
joao Pedro <jaoopedro.dev@gmail.com>
2022-02-05 11:42:39 -0300

    Utilização do if elif e else e while para a repetição

---
+++ main.py
@@ -0,0 +1,59 @@
+
+print('Bem Vindo a calculadora')
+
+print()
+
+num1 = float(input('Digite o primeiro numero: '))
+num2 = float(input(' Digite o segundo numero: '))
+
+con = input('Digite o tipo de conta que ira fazer (+,-,*,/,%): ')
+
+
+if con == '+':
+    print('Sua Resposta é')
+    print(round(num1 + num2,2))
+
+elif con == '-':
+    print('Sua Resposta é')
+    print(round(num1 - num2,2))
+
+elif con == '*':
+    print('Sua Resposta é')
+    print(round(num1 * num2,2))
+
+elif con == '/':
+    print('Sua Resposta é')
+    print(round(num1/num2,2))
+
+elif con == '%':
+    print('Sua Resposta é')
+    print(round(num1 % num2))
+
+else:
+    print('Resposta invalida')
+
+    while con != '+' and con != '-' and con != '*' and con != '/' and con != '%':
+        # Repetição caso a pessoa expecifique errado o tipo de conta
+
+        con = input('Digite o tipo de conta que ira fazer (+,-,*,/,%): ')
+
+        if con == '+':
+            print('Sua Resposta é')
+            print(round(num1 + num2, 2))
+
+        elif con == '-':
+            print('Sua Resposta é')
+            print(round(num1 - num2, 2))
+
+        elif con == '*':
+            print('Sua Resposta é')
+            print(round(num1 * num2, 2))
+
+        elif con == '/':
+            print('Sua Resposta é')
+            print(round(num1 / num2, 2))
+
+        elif con == '%':
+            print('Sua Resposta é')
+            print(round(num1 % num2))
+
